dcfe934abd1018ee5f3cb6efda83a441af59110f
GaneshKhantwal <ganesh.khantwal@niit-tech.com>
2015-07-05 16:41:07 +0530

    add dev project

---
+++ WebPlusLogicWithAPIApps-Dev.Business/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPlusLogicWithAPIApps_Dev.Business
+{
+    public class DBConnection
+    {
+        private static string connectionstring;
+
+        public static string ConnectionString
+        {
+            get {
+                connectionstring = ConfigurationManager.ConnectionStrings["DBConnection"].ToString();
+                return connectionstring; 
+            }
+            set { connectionstring = value; }
+        }
+       
+    }
+}
---
+++ WebPlusLogicWithAPIApps-Dev.Business/OrderBusiness.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.ApplicationBlocks.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebPlusLogicWithAPIApps_Dev.Entity;
+
+namespace WebPlusLogicWithAPIApps_Dev.Business
+{
+    public class OrderBusiness
+    {
+        public BusinessResult Create(OrderEntity orderEntity)
+        {
+            List<SqlParameter> paramList = new List<SqlParameter>();
+            paramList.Add(new SqlParameter() { ParameterName = "@RecipientName", SqlValue=orderEntity.RecipientName });
+            paramList.Add(new SqlParameter() { ParameterName = "@DeliveryStreet", SqlValue = orderEntity.DeliveryStreet });
+            paramList.Add(new SqlParameter() { ParameterName = "@DeliveryCity", SqlValue = orderEntity.DeliveryCity });
+            paramList.Add(new SqlParameter() { ParameterName = "@DeliveryState", SqlValue = orderEntity.DeliveryState });
+            paramList.Add(new SqlParameter() { ParameterName = "@DeliveryPostalCode", SqlValue = orderEntity.DeliveryPostalCode });
+            paramList.Add(new SqlParameter() { ParameterName = "@Phone", SqlValue = orderEntity.Phone});
+            paramList.Add(new SqlParameter() { ParameterName = "@Email", SqlValue = orderEntity.Email });
+            paramList.Add(new SqlParameter() { ParameterName = "@OrderId", SqlValue = orderEntity.OrderId, Direction = System.Data.ParameterDirection.Output });
+            int result = SqlHelper.ExecuteNonQuery(DBConnection.ConnectionString, System.Data.CommandType.StoredProcedure, "usp_CreateOrder", paramList.ToArray());
+            
+            return new BusinessResult(){ISuccess=true,Code="S",Message="Order Created Successfully."};
+        }
+    }
+}
